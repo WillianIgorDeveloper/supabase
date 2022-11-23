046c643baa4c964885c16443fbef3ca175987f5a
Willian Igor <willianigordeveloper@gmail.com>
2022-11-23 15:04:01 -0300

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
 import { useEffect, useState } from 'react'
+import { supabase } from './supabaseClient'
 
 export const App = () => {
-
-   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-   const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-   const supabase = createClient(supabaseUrl, supabaseKey)
 
    const [coments, setComents] = useState([])
 
